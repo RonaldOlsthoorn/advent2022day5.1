0e6db550d2cef69fcf6a937fba047eca61d50c15
Hasdrubal <ronaldolsthoorn1@gmail.com>
2022-12-17 12:07:59 +0100

    working

--- src/main.rs
+++ src/main.rs
@@ -2,104 +2,71 @@ use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use std::collections::{HashMap, HashSet};
 
-fn calculate_score(outcome: i32, opponent_play: i32)-> i32{
-
-    // -1 for lose, 0 for draw 1 for win
-
-    let mut diff = outcome + opponent_play;
-    let mut own_play = diff;
-
-    if own_play > 2 {
-        own_play = 0;
-    } else if own_play < 0 {
-        own_play = 2
-    }
-
-    println!("own_play {} opponent_play {} outcome {} score: {}", own_play, opponent_play, outcome, 1 + own_play + 3 * (outcome + 1));
-    1 + own_play + 3 * (outcome + 1)
-}
 
 fn main() {
-    let file = File::open("input.txt").unwrap();
-    let reader = BufReader::new(file);
+    let reader = BufReader::new(File::open("input.txt").unwrap());
 
-    let mut scoreMap = HashMap::new();
-    scoreMap.insert('a', 1);
-    scoreMap.insert('b', 2);
-    scoreMap.insert('c', 3);
-    scoreMap.insert('d', 4);
-    scoreMap.insert('e', 5);
-    scoreMap.insert('f', 6);
-    scoreMap.insert('g', 7);
-    scoreMap.insert('h', 8);
-    scoreMap.insert('i', 9);
-    scoreMap.insert('j', 10);
-    scoreMap.insert('k', 11);
-    scoreMap.insert('l', 12);
-    scoreMap.insert('m', 13);
-    scoreMap.insert('n', 14);
-    scoreMap.insert('o', 15);
-    scoreMap.insert('p', 16);
-    scoreMap.insert('q', 17);
-    scoreMap.insert('r', 18);
-    scoreMap.insert('s', 19);
-    scoreMap.insert('t', 20);
-    scoreMap.insert('u', 21);
-    scoreMap.insert('v', 22);
-    scoreMap.insert('w', 23);
-    scoreMap.insert('x', 24);
-    scoreMap.insert('y', 25);
-    scoreMap.insert('z', 26);
+    let mut emptyLineNumber = 0;
 
-    scoreMap.insert('A', 27);
-    scoreMap.insert('B', 28);
-    scoreMap.insert('C', 29);
-    scoreMap.insert('D', 30);
-    scoreMap.insert('E', 31);
-    scoreMap.insert('F', 32);
-    scoreMap.insert('G', 33);
-    scoreMap.insert('H', 34);
-    scoreMap.insert('I', 35);
-    scoreMap.insert('J', 36);
-    scoreMap.insert('K', 37);
-    scoreMap.insert('L', 38);
-    scoreMap.insert('M', 39);
-    scoreMap.insert('N', 40);
-    scoreMap.insert('O', 41);
-    scoreMap.insert('P', 42);
-    scoreMap.insert('Q', 43);
-    scoreMap.insert('R', 44);
-    scoreMap.insert('S', 45);
-    scoreMap.insert('T', 46);
-    scoreMap.insert('U', 47);
-    scoreMap.insert('V', 48);
-    scoreMap.insert('W', 49);
-    scoreMap.insert('X', 50);
-    scoreMap.insert('Y', 51);
-    scoreMap.insert('Z', 52);
-
-    let mut letterBins = HashSet::new();
-    let mut foundLetterTypes = HashSet::new();
-
-    let mut score = 0;
-    for line in reader.lines() {
-        let l = line.unwrap();
-        println!("line: {}", l);
-
-        for (i, c) in l.chars().enumerate(){
-            if i < l.len() / 2 {
-                letterBins.insert(c);
-            } else{
-                if letterBins.contains(&c) && !foundLetterTypes.contains((&c)) {
-                    score += *scoreMap.get(&c).unwrap();
-                    foundLetterTypes.insert(c);
-                    println!("found character: {} in both compartments", c);
-                }
-            }
+    for (i, line) in reader.lines().enumerate() {
+        if line.unwrap().is_empty() {
+            emptyLineNumber = i;
         }
-        letterBins.clear();
-        foundLetterTypes.clear();
     }
 
-    println!("total score {}", score);
+    let reader = BufReader::new(File::open("input.txt").unwrap());
+
+    let lines: Vec<String>= reader.lines().map(|l| l.unwrap()).collect();
+    let stackRegion = &lines[0..emptyLineNumber];
+    let stackNumber = stackRegion[emptyLineNumber - 1].split_whitespace().collect::<Vec<&str>>().len();
+
+    let mut stacks :Vec<Vec<char>> = Vec::new();
+
+    for i in 0..stackNumber {
+        stacks.push(Vec::new());
+    }
+
+    for  line in stackRegion[0..(stackRegion.len()-1)].iter().rev() {
+        let mut characterIndex = 1;
+        let mut stackIndex = 0;
+
+        while characterIndex < line.len() {
+
+            if line.chars().nth(characterIndex).unwrap() != ' '{
+                stacks[stackIndex].push(line.chars().nth(characterIndex).unwrap());
+            }
+
+            characterIndex += 4;
+            stackIndex += 1;
+        }
+    }
+
+    for line in &lines[emptyLineNumber+1..] {
+
+        println!("line: {}", line);
+
+        let elements = line.split_whitespace().collect::<Vec<&str>>();
+        let moveAmount = elements[1].parse::<i32>().unwrap();
+        let moveFrom = elements[3].parse::<i32>().unwrap() - 1;
+        let moveTo = elements[5].parse::<i32>().unwrap() - 1;
+
+        for _ in 0..moveAmount {
+            let element = stacks[moveFrom as usize].pop().unwrap();
+            stacks[moveTo as usize].push(element);
+        }
+
+        for s in &stacks{
+
+            let mut printstack: String = "[".to_string();
+            for e in s{
+                printstack.push(*e);
+            }
+            printstack +="]";
+
+            println!("stack: {}", printstack);
+
+        }
+    }
+
+    //let numberOfStacks = lines[emptyLineNumber -1].unwrap().split(" ").collect().len();
 }
